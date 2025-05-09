0996a3d9a7ac2292fd86800f471f7cf6568bd5d4
Sebas <saabad@pucp.edu.pe>
2025-05-08 20:41:38 -0500

    commit1

---
+++ src/main/java/com/example/lab4_gtics_20251_20206464/Lab4Gtics2025120206464Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_gtics_20251_20206464;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4Gtics2025120206464Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4Gtics2025120206464Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab4_gtics_20251_20206464/Lab4Gtics2025120206464ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_gtics_20251_20206464;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4Gtics2025120206464ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
